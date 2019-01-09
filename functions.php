4caedc85e01e1b93d78f871bdac401fa9b9be41c
Louise <louise.semendjan@orange.fr>
2019-01-09 16:15:53 +0100

    update function.php with all the custom post type (members / FAQ / events / materiel / creations

--- functions.php
+++ functions.php
@@ -8,6 +8,8 @@
 // );
 // }
 // add_action( 'init', 'enregistre_menus' );
+
+//Membres
 add_action('init', 'register_cpt_membre');
 
 function register_cpt_membre()
@@ -49,5 +51,177 @@ function register_cpt_membre()
 	);
 	register_post_type('membre', $args);
 	}
+
+  //FAQ
+  add_action('init', 'register_cpt_question');
+
+  function register_cpt_question()
+  	{
+  	$labels = array(
+  		'name' => _x('Questions', 'question') ,
+  		'singular_name' => _x('Questions', 'question') ,
+  		'add_new' => _x('Ajouter', 'question') ,
+  		'add_new_item' => _x('Ajouter Hotel', 'question') ,
+  		'edit_item' => _x('Editer Hotel', 'question') ,
+  		'new_item' => _x('Nouveau Hotel', 'question') ,
+  		'view_item' => _x('Voir Hotel', 'question') ,
+  		'search_items' => _x('Rechercher Questions', 'question') ,
+  		'not_found' => _x('Aucun Hotel', 'question') ,
+  		'not_found_in_trash' => _x('Aucun Hotel dans la Corbeille', 'question') ,
+  		'parent_item_colon' => _x('Parent Hotel:', 'question') ,
+  		'menu_name' => _x('Questions', 'question') ,
+  	);
+  	$args = array(
+  		'labels' => $labels,
+  		'hierarchical' => false,
+  		'supports' => array(
+  			'title',
+  			'editor'
+  		) ,
+  		'public' => true,
+  		'show_ui' => true,
+  		'show_in_menu' => true,
+  		'menu_position' => 5,
+  		'menu_icon' => 'dashicons-format-quote',
+  		'show_in_nav_menus' => true,
+  		'publicly_queryable' => true,
+  		'exclude_from_search' => false,
+  		'has_archive' => true,
+  		'query_var' => true,
+  		'can_export' => true,
+  		'rewrite' => true,
+  		'capability_type' => 'post'
+  	);
+  	register_post_type('question', $args);
+  	}
+
+    //EVENEMENTS
+    add_action('init', 'register_cpt_evenement');
+
+    function register_cpt_evenement()
+    	{
+    	$labels = array(
+    		'name' => _x('Evenements', 'evenement') ,
+    		'singular_name' => _x('Evenements', 'evenement') ,
+    		'add_new' => _x('Ajouter', 'evenement') ,
+    		'add_new_item' => _x('Ajouter Hotel', 'evenement') ,
+    		'edit_item' => _x('Editer Hotel', 'evenement') ,
+    		'new_item' => _x('Nouveau Hotel', 'evenement') ,
+    		'view_item' => _x('Voir Hotel', 'evenement') ,
+    		'search_items' => _x('Rechercher Evenements', 'evenement') ,
+    		'not_found' => _x('Aucun Hotel', 'evenement') ,
+    		'not_found_in_trash' => _x('Aucun Hotel dans la Corbeille', 'evenement') ,
+    		'parent_item_colon' => _x('Parent Hotel:', 'evenement') ,
+    		'menu_name' => _x('Evenements', 'evenement') ,
+    	);
+    	$args = array(
+    		'labels' => $labels,
+    		'hierarchical' => false,
+    		'supports' => array(
+    			'title',
+    			'editor'
+    		) ,
+    		'public' => true,
+    		'show_ui' => true,
+    		'show_in_menu' => true,
+    		'menu_position' => 5,
+    		'menu_icon' => 'dashicons-format-quote',
+    		'show_in_nav_menus' => true,
+    		'publicly_queryable' => true,
+    		'exclude_from_search' => false,
+    		'has_archive' => true,
+    		'query_var' => true,
+    		'can_export' => true,
+    		'rewrite' => true,
+    		'capability_type' => 'post'
+    	);
+    	register_post_type('evenement', $args);
+    	}
+
+      //MATERIEL
+      add_action('init', 'register_cpt_materiel');
+
+      function register_cpt_materiel()
+      	{
+      	$labels = array(
+      		'name' => _x('Materiel', 'materiel') ,
+      		'singular_name' => _x('Materiel', 'materiel') ,
+      		'add_new' => _x('Ajouter', 'materiel') ,
+      		'add_new_item' => _x('Ajouter Hotel', 'materiel') ,
+      		'edit_item' => _x('Editer Hotel', 'materiel') ,
+      		'new_item' => _x('Nouveau Hotel', 'materiel') ,
+      		'view_item' => _x('Voir Hotel', 'materiel') ,
+      		'search_items' => _x('Rechercher Materiel', 'materiel') ,
+      		'not_found' => _x('Aucun Hotel', 'materiel') ,
+      		'not_found_in_trash' => _x('Aucun Hotel dans la Corbeille', 'materiel') ,
+      		'parent_item_colon' => _x('Parent Hotel:', 'materiel') ,
+      		'menu_name' => _x('Materiel', 'materiel') ,
+      	);
+      	$args = array(
+      		'labels' => $labels,
+      		'hierarchical' => false,
+      		'supports' => array(
+      			'title',
+      			'editor'
+      		) ,
+      		'public' => true,
+      		'show_ui' => true,
+      		'show_in_menu' => true,
+      		'menu_position' => 5,
+      		'menu_icon' => 'dashicons-format-quote',
+      		'show_in_nav_menus' => true,
+      		'publicly_queryable' => true,
+      		'exclude_from_search' => false,
+      		'has_archive' => true,
+      		'query_var' => true,
+      		'can_export' => true,
+      		'rewrite' => true,
+      		'capability_type' => 'post'
+      	);
+      	register_post_type('materiel', $args);
+      	}
+
+        //Réalisations
+        add_action('init', 'register_cpt_realisation');
+
+        function register_cpt_realisation()
+        	{
+        	$labels = array(
+        		'name' => _x('Realisations', 'realisation') ,
+        		'singular_name' => _x('Realisations', 'realisation') ,
+        		'add_new' => _x('Ajouter', 'realisation') ,
+        		'add_new_item' => _x('Ajouter Hotel', 'realisation') ,
+        		'edit_item' => _x('Editer Hotel', 'realisation') ,
+        		'new_item' => _x('Nouveau Hotel', 'realisation') ,
+        		'view_item' => _x('Voir Hotel', 'realisation') ,
+        		'search_items' => _x('Rechercher Realisations', 'realisation') ,
+        		'not_found' => _x('Aucun Hotel', 'realisation') ,
+        		'not_found_in_trash' => _x('Aucun Hotel dans la Corbeille', 'realisation') ,
+        		'parent_item_colon' => _x('Parent Hotel:', 'realisation') ,
+        		'menu_name' => _x('Realisations', 'realisation') ,
+        	);
+        	$args = array(
+        		'labels' => $labels,
+        		'hierarchical' => false,
+        		'supports' => array(
+        			'title',
+        			'editor'
+        		) ,
+        		'public' => true,
+        		'show_ui' => true,
+        		'show_in_menu' => true,
+        		'menu_position' => 5,
+        		'menu_icon' => 'dashicons-format-quote',
+        		'show_in_nav_menus' => true,
+        		'publicly_queryable' => true,
+        		'exclude_from_search' => false,
+        		'has_archive' => true,
+        		'query_var' => true,
+        		'can_export' => true,
+        		'rewrite' => true,
+        		'capability_type' => 'post'
+        	);
+        	register_post_type('realisation', $args);
+        	}
 
 ?>
