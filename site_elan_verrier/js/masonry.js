b763d64f0b8f0adb6410d0bce35fc61df46c87f0
Louise <louise.semendjan@orange.fr>
2019-03-06 12:56:08 +0100

    add realisation and modification index

--- site_elan_verrier/js/masonry.js
+++ site_elan_verrier/js/masonry.js
@@ -6,11 +6,11 @@ $(document).ready(() => {
   percentPosition: true
   });
 
-  $grid.on( 'click', function(ev) {
-    ev.target.parentNode.classList = "grid-item col-sm-6";
-    ev.target.style.zIndex = 2;
-
-  })
+  // $grid.on( 'click', function(ev) {
+  //   ev.target.parentNode.classList = "grid-item col-sm-6";
+  //   ev.target.style.zIndex = 2;
+  //
+  // })
 
 
 })
