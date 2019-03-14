134c9b325c7828c2195d9bc7212522217d16632e
Valentinrab <valentinrabier@outlook.com>
2019-03-14 15:03:38 +0100

    wp footer

--- footer.php
+++ footer.php
@@ -2,15 +2,31 @@
   <footer class="content_all">
     <section class="ft">
 
-      <section class="container-top">
-          <p class="ft-ctn-sn">Facebook</p>
-          <p class="ft-ctn-sn">Instagram</p>
-          <p class="ft-ctn-sn">Twitter</p>
-      </section>
+      <?php
+
+      // check if the repeater field has rows of data
+      if( have_rows('footer_reseaux') ):
+
+       	// loop through the rows of data
+          while ( have_rows('footer_reseaux') ) : the_row(); ?>
+
+
+          <section class="container-top">
+              <a href="<?php the_sub_field('reseaux_lien'); ?>"><p class="ft-ctn-sn"><?php the_sub_field('reseaux_icone'); ?></p></a>
+          </section>
+
+      <?php
+          endwhile;
+          endif;
+
+      ?>
+
+
+
 
       <section class="container-bottom">
-          <a href="" class="ft-ctn-mention">MENTIONS LÉGALES</a>
-          <a href="" class="ft-ctn-mention">NOUS CONTACTER</a>
+          <a href="<?php echo get_permalink(107); ?>" class="ft-ctn-mention">MENTIONS LÉGALES</a>
+          <a href="<?php echo get_permalink(63); ?>" class="ft-ctn-mention">NOUS CONTACTER</a>
       </section>
 
     </section>
