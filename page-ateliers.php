16cdbc6f746e4407cd80d3529a021803a0c8984f
Valentinrab <valentinrabier@outlook.com>
2019-03-14 11:04:12 +0100

    wp atelier

--- page-ateliers.php
+++ page-ateliers.php
@@ -27,20 +27,34 @@
         <div class="title-container">
           <h1 class="main-title">Les ateliers</h1>
         </div>
-        <div class="row" id="row-glob">
-          <div class="col-12 col-lg-6" id="col-left" style="background-image: url(<?php bloginfo('template_directory'); ?>/images/atelier1.jpg);">
+        <?php
 
-          </div>
-          <div class="col-12 col-lg-6" id="col-right">
-            <div class="offset-1 col-10 col-md-8 offset-md-2 col-lg-8 offset-lg-2 col-xl-8 offset-xl-2">
-              <h2 class="title-atelier">Verre à chaud</h2>
-              <p class="p-atelier">Équipé de deux fours de fusion à 1200° C, d'arches de recuisson (fours où l'on dépose les pièces après le soufflage) et d'équipements de réalisation à chaud. Ce plateau technique voit les maîtres verriers transmettre leur
-                savoir-faire
-                du CAP au DMA avec passion.
-                Il n'est pas rare d'apercevoir des artistes assister les souffleurs de verre à la modélisation de leurs idées, dans une collaboration et un échange avec les étudiants.</p>
+        // check if the repeater field has rows of data
+        if( have_rows('ateliers') ):
+
+         	// loop through the rows of data
+            while ( have_rows('ateliers') ) : the_row(); ?>
+            <div class="row" id="row-glob">
+              <div class="col-12 col-lg-6" id="col-left" style="background-image: url(<?php the_sub_field('ateliers_image'); ?>);">
+
+              </div>
+              <div class="col-12 col-lg-6" id="col-right">
+                <div class="offset-1 col-10 col-md-8 offset-md-2 col-lg-8 offset-lg-2 col-xl-8 offset-xl-2">
+                  <h2 class="title-atelier"><?php the_sub_field('ateliers_titre_atelier'); ?></h2>
+                  <p class="p-atelier"><?php the_sub_field('ateliers_description'); ?></p>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
+
+                <!-- // code html avec pour les zones administrables -->
+
+<?php
+    endwhile;
+    endif;
+
+?>
+
+
       </main>
     </section>
   </section>
