13596c481f97b2aff86bec767bfd9dc0cb8f2ec6
Louise <louise.semendjan@orange.fr>
2019-03-22 22:13:17 +0100

    wp test remove arrows slider

--- page-ateliers.php
+++ page-ateliers.php
@@ -56,6 +56,7 @@
   ?>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="<?php bloginfo('template_directory'); ?>/lib/eoko/eoko.js"></script>
+  <script src="<?php bloginfo('template_directory'); ?>/lib/eoko/arrows.js"></script>
   <script src="https://unpkg.com/scrollreveal"></script>
   <script src="<?php bloginfo('template_directory'); ?>/js/main.js"></script>
 </head>
