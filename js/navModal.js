8a2cf1b3b330c5ea66c9fc8886d066e92dbfd044
Louise <louise.semendjan@orange.fr>
2019-03-23 19:36:30 +0100

    wp : add illustration + update agenda pop up

--- js/navModal.js
+++ js/navModal.js
@@ -6,14 +6,12 @@ var currentModal = $(this);
 
 //click next
 currentModal.find('.right').click(function(){
-  console.log('hey');
   currentModal.modal('hide');
   currentModal.closest("div[id^='myModal']").nextAll("div[id^='myModal']").first().modal('show');
 });
 
 //click prev
 currentModal.find('.left').click(function(){
-  console.log('hey');
   currentModal.modal('hide');
   currentModal.closest("div[id^='myModal']").prevAll("div[id^='myModal']").first().modal('show');
 });
--- page-agenda.php
+++ page-agenda.php
@@ -17,7 +17,6 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://unpkg.com/scrollreveal"></script>
   <script src="<?php bloginfo('template_directory'); ?>/js/main.js"></script>
   <script src = "<?php bloginfo('template_directory'); ?>/js/navModal.js"></script>
