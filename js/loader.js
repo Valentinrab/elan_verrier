c55887cf59180523ae62cce69fd38d1ffa87153b
Louise <louise.semendjan@orange.fr>
2019-03-24 16:36:55 +0100

    wp update contact

--- js/loader.js
+++ js/loader.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
   setTimeout(function(){$("#content_svg").fadeOut();}, 2500);
   setTimeout(function(){$(".bg-blue").fadeOut("slow");}, 2900);
-  // setTimeout(function(){$(".bg-orange").fadeOut("slow");}, 5500);
-  // setTimeout(function(){$(".bg-pink").fadeOut("slow");}, 6000);
   setTimeout(function(){$(".loader").fadeOut();}, 4300);
 
 });
