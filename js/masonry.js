bfba5b3cd06b021424f6e02f631abbb159713ad6
Louise <louise.semendjan@orange.fr>
2019-03-25 18:39:44 +0100

    wp : last modif

--- js/masonry.js
+++ js/masonry.js
@@ -1,42 +1,32 @@
-// // external js: masonry.pkgd.js
-// $(window).on("load", function (e) {
-//
-//   var $grid = $('.grid').masonry({
-//   itemSelector: '.grid-item',
-//   columnWidth: '.grid-sizer',
-//   percentPosition: true
-//   });
-//
-//   document.getElementById('grid').style.opacity = 1;
-//   // $grid.style.opacity = 1;
-//   // $('.grid').imagesLoaded().progress( function() {
-//   //   $('.grid').masonry('layout');
-//   // });
-//
-//
-//
-//
-// });
+// external js: masonry.pkgd.js
+$(window).on("load", function (e) {
 
-var $grid = $('.grid').masonry({
-  itemSelector: 'none', // select none at first
+  var $grid = $('.grid').masonry({
+  itemSelector: 'none',
   columnWidth: '.grid-sizer',
-  // gutter: '.grid__gutter-sizer',
   percentPosition: true,
-  stagger: 30,
+  stagger: 300,
   // nicer reveal transition
   visibleStyle: { transform: 'translateY(0)', opacity: 1 },
-  hiddenStyle: { transform: 'translateY(100px)', opacity: 0 },
-});
+  hiddenStyle: { transform: 'translateY(50px)', opacity: 0 }
+  });
 
-// get Masonry instance
-var msnry = $grid.data('masonry');
-
-// initial items reveal
+  // initial items reveal
 $grid.imagesLoaded( function() {
   $grid.removeClass('are-images-unloaded');
   $grid.masonry( 'option', { itemSelector: '.grid-item' });
   var $items = $grid.find('.grid-item');
   $grid.masonry( 'appended', $items );
 });
-// document.getElementById('grid').style.opacity = 1;
+
+
+  // document.getElementById('grid').style.opacity = 1;
+  // $grid.style.opacity = 1;
+  // $('.grid').imagesLoaded().progress( function() {
+  //   $('.grid').masonry('layout');
+  // });
+
+
+
+
+});
